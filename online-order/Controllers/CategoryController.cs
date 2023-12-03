a416f8f0423e3b9160849d380bc6cb81ed336459
Akmal <akamolov@eaglebusinesssoftware.com>
2023-12-03 13:26:23 -0500

    ability to get a value using CategoryController

--- online-order/Controllers/CategoryController.cs
+++ online-order/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using online_order.Data;
+using online_order.Models;
 
 namespace online_order.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> objList = _db.Category;
+            return View(objList);
         }
     }
 }
